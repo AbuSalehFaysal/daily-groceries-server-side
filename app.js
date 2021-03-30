e184ec9f38920b00ca932a1d25546bf2dfe4e63d
DESKTOP-UQSIAMQ\User <asfaysal.bracu@gmail.com>
2021-03-31 03:16:12 +0600

    Image URL Added

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
+const cors = require('cors')
 const ObjectId = require('mongodb').ObjectId;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cors());
 
 const password = '8uftxmhexuEIGnTJ';
 
